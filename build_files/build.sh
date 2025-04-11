1c9c82e2173c86be3551e90660f57714f43b0725
thesaigoneer <165976396+thesaigoneer@users.noreply.github.com>
2025-04-11 14:53:01 +0700

    Update build.sh - 1st package set

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,14 +10,24 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 install -y eza 
+dnf5 install -y fastfetch
+dnf5 install -y mc
+dnf5 install -y micro
+dnf5 install -y zsh
+dnf5 install -y zsh-autosuggestions
+dnf5 install -y gnome-shell-extension-appindicator
+dnf5 install -y gnome-shell-extension-blur-my-shell
+dnf5 install -y gnome-shell-extension-caffeine
+dnf5 install -y gnome-shell-extension-no-overview
+dnf5 install -y gnome-shell-extension-places-menu
+dnf5 remove -y  firefox
+dnf5 remove -y  gnome-software
+dnf5 remove -y  gnome-tour
+dnf5 remove -y  gnome-tweaks
+dnf5 remove -y  malcontent
+dnf5 remove -y  nvtop
+dnf5 remove -y  yelp
 
 #### Example for enabling a System Unit File
 
