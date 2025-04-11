ce462c4269def9988a6855710b9055be6ab35744
thesaigoneer <165976396+thesaigoneer@users.noreply.github.com>
2025-04-11 15:06:16 +0700

    Update build.sh - pkg eza removed; unmaintained in F42

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,6 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y eza 
 dnf5 install -y fastfetch
 dnf5 install -y mc
 dnf5 install -y micro
