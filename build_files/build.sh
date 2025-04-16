6ce76faae6db11b41f00c647eb23107707358c5c
thesaigoneer <165976396+thesaigoneer@users.noreply.github.com>
2025-04-16 17:51:04 +0700

    Update build.sh - building on top of the base image

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,12 +11,27 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 install -y fastfetch
+dnf5 install -y gdm
+dnf5 install -y gnome-control-center
+dnf5 install -y gnome-session-wayland-session
+dnf5 install -y gnome-shell
+dnf5 install -y gvfs
+dnf5 install -y gvfs-archive
+dnf5 install -y gvfs-client
+dnf5 install -y gvfs-fuse
+dnf5 install -y gvfs-mtp
+dnf5 install -y gvfs-smb
 dnf5 install -y mc
-dnf5 install -y micro
+dnf5 install -y nautilus
+dnf5 install -y nautilus-extensions
+dnf5 install -y ptyxis
 dnf5 install -y zsh
 dnf5 install -y zsh-autosuggestions
-dnf5 remove -y  firefox
-dnf5 remove -y  nvtop
+
+dnf5 remove -y firefox
+dnf5 remove -y gnome-tour
+dnf5 remove -y hplip
+dnf5 remove -y nvtop
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
